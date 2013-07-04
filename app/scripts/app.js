1ebbe6f74f3c4904d16191bd7d1821ab3f210f77
Hiroki Gota <gotahiroki@gmail.com>
2013-07-04 20:06:32 +1000

    initial

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('pms3App', [])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
