8a82b5ea58236a5769cbb7be6ffeceaa73e90145
Hiroki Gota <gotahiroki@gmail.com>
2013-09-25 21:32:26 +1000

    prop details element done

--- app/scripts/services/reportService.js
+++ app/scripts/services/reportService.js
@@ -15,6 +15,19 @@ angular.module('pms3App')
           return new Date(0, 0, 0, 0, 0, 0).setFullYear(year, 6, 1);
         }
 
+        $scope.viewing = {address:'29 Dover St RICHMOND 3121 VIC'};
+
+
+//        2007 Market Value
+//        $425,000
+//        $560,000
+//        $550,000
+//        $645,000
+//        $645,000
+//        $645,000
+//        $650,000
+
+
         $scope.years = [];
 //        for (var i = 2007; i <= 2013; i++) {
 //          $scope.years.push(createYear(i));
