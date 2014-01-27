87e463c404e4371e9e121230c12d7e9106025ef9
Hiroki Gota <gotahiroki@gmail.com>
2014-01-27 12:37:44 +1100

    details done

--- app/scripts/controllers/viewProperty.js
+++ app/scripts/controllers/viewProperty.js
@@ -11,6 +11,15 @@ angular.module('pms3App')
         'Residential (Unit)',
         'Commercial/Industrial',
         'Residential'];
+      $scope.states = ['VIC',
+        'NSW',
+        'QLD',
+        'SA',
+        'TAS',
+        'NT',
+        'WA'
+      ];
+      $scope.numBedRooms = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 
       $log.info('ViewPropertyCtrl code: ' + code);
 
