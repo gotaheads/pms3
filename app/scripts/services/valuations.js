e717ec1b8c2b683aae506664dcba69f7a1f3f1c1
Hiroki Gota <gotahiroki@gmail.com>
2014-04-21 22:19:18 +1000

    orig equity

--- app/scripts/services/valuations.js
+++ app/scripts/services/valuations.js
@@ -11,6 +11,7 @@ angular.module('pms3App')
       var propsByClientCode = {};
       var marketValuesByPropCode = {};
       var annualRentByPropCode = {};
+      var origMortgageByPropCode = {};
       var marketMediansBySuburb = {};
 
       function loadClientProps(properties) {
@@ -58,6 +59,27 @@ angular.module('pms3App')
         $log.info('loadPropertyAnnualRent: ' + annualRentByCode.length);
         return annualRentByCode;
       }
+      function findAnnualRentByCode(p_code) {
+        var val = annualRentByPropCode[p_code];
+        return !!val?val:0;
+      }
+
+      function loadPropertyOrigMortgage(mortgages) {
+        var mortgageByCode = {};
+        mortgages.forEach(function(i) {
+          if(mortgageByCode[i.p_code] === undefined) {
+            var r = i.p_mortgage;
+            mortgageByCode[i.p_code] = (!!r?r:0);
+          }
+        });
+        $log.info('loadPropertyOrigMortgage: ' + mortgageByCode.length);
+        return mortgageByCode;
+      }
+
+      function findOrigMortgageByCode(p_code) {
+        var val = origMortgageByPropCode[p_code];
+        return !!val?val:0;
+      }
 
 
       function loadMarketMedians(marketMedians) {
@@ -88,15 +110,18 @@ angular.module('pms3App')
         return p;
       }
 
-      function findAnnualRentByCode(p_code) {
-        var val = annualRentByPropCode[p_code];
-        return !!val?val:0;
-      }
-
       function initAnnualRent(p) {
         p.annualRent = findAnnualRentByCode(p.pcode);
         if(p.annualRent > 0) {
           p.rentalGrowth = (p.annualRent/p.p_origcost) * 100;
+        }
+        return p;
+      }
+
+      function initOrigMortgage(p) {
+        p.origMortgage = findOrigMortgageByCode(p.pcode);
+        if(p.origMortgage > 0) {
+          p.oriNetEty = (p.p_origcost - p.origMortgage);
         }
 
         return p;
@@ -127,6 +152,7 @@ angular.module('pms3App')
 
           client.properties.forEach(function(p) {
             client.totalOriginalCost += p.p_origcost;
+            p = initOrigMortgage(p);
             p = initMarketValues(p);
             p = initAnnualRent(p);
 
@@ -149,6 +175,8 @@ angular.module('pms3App')
           marketValuesByPropCode = loadPropertyMarketValues($scope.rests.convertItems(data.marketValues));
 
           annualRentByPropCode = loadPropertyAnnualRent($scope.rests.convertItems(data.monthlyRents));
+
+          origMortgageByPropCode = loadPropertyOrigMortgage($scope.rests.convertItems(data.mortgages));
 
           marketMediansBySuburb = loadMarketMedians($scope.rests.convertItems(data.marketMedians));
 
