a073b6c0d16431351ed966396f6cc453bc3f55ea
Hiroki Gota <gotahiroki@gmail.com>
2014-06-08 21:50:09 +1000

    style

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,16 +3,25 @@
 angular.module('pms3App')
   .controller('MainCtrl', ['$scope', '$log', 'propertyService',
     function ($scope, $log, propertyService) {
-    $scope.code= 'ABEL001';
-    $scope.selected = '';
+    //$scope.code= 'ABEL001';
+    $scope.selectedProperty = '',
+    $scope.selectedClient = '';
 
     propertyService.loadCodes($scope);
 
     $scope.loadProperty = function() {
-      $log.info('loadProperty: ' + $scope.selected.length)
+      $log.info('loadProperty: ' + $scope.selectedProperty.length)
 
-      if($scope.selected.length == 10) {
-        $scope.viewProperty($scope.selected);
+      if($scope.selectedProperty.length == 10) {
+        $scope.viewProperty($scope.selectedProperty);
       }
     }
+    $scope.loadClient = function() {
+      $log.info('loadClient: ' + $scope.selectedClient.length)
+
+      if($scope.selectedClient.length == 10) {
+        $scope.viewProperty($scope.selectedClient);
+      }
+    }
+
   }]);
