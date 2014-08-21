4b80d11093b85b003689ff6a26a88609d106535c
Hiroki Gota <gotahiroki@gmail.com>
2014-08-21 21:16:30 +1000

    removed unfinished

--- app/scripts/services/propertyService.js
+++ app/scripts/services/propertyService.js
@@ -6,34 +6,6 @@ angular.module('pms3App')
       var codes;
       var editing = {};
 
-//      function convertItems(data) {
-//        var props = data.COLUMNS;
-//        var rows = data.DATA;
-//        var items = [];
-//        for(var ri = 0; ri < rows.length; ri++) {
-//          var values = rows[ri];
-//          var item = {};
-//          for(var i = 0; i < props.length; i++) {
-//            var prop = props[i].toLowerCase();
-//            item[prop] = values[i];
-//          }
-//          items.push(item);
-//        }
-//        return items;
-//
-//      }
-//
-//      function convertItem(data) {
-//        var props = data.COLUMNS;
-//        var values = data.DATA[0];
-//        var property = {}
-//        for(var i = 0; i < props.length; i++) {
-//          var prop = props[i].toLowerCase();
-//          property[prop] = values[i];
-//        }
-//        return property;
-//      }
-
       function find(items, prop, val) {
         for(var i = 0; i < items.length; i++) {
           var item = items[i];
