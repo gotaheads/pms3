f81996e10ecf3b3b5467543ebf502a64f4f236fc
Hiroki Gota <gotahiroki@gmail.com>
2014-09-02 21:30:37 +1000

    per page done

--- app/scripts/controllers/valuationReport.js
+++ app/scripts/controllers/valuationReport.js
@@ -1,13 +1,59 @@
 'use strict';
 
 angular.module('pms3App')
-  .controller('ValuationReportCtrl', ['$scope', 'reportService',
-  function ($scope, reportService) {
+  .controller('ValuationReportCtrl',
+  function ($scope, $log, reportService) {
     reportService.load($scope);
+
     $scope.toDate = function(d) {
       return new Date(d);
     }
-    $scope.pageBreak = function(idx) {
+    function isOdd(num) { return num % 2;}
 
+    $scope.page = function(idx, c, cidx) {
+      var count = c.properties.length,
+        pages = parseInt(count),
+        current = (idx+1)
+        ;
+
+//      $log.info('count  ' + count + ' idx ' + idx + isOdd(idx) +
+//      ' pages ' + pages);
+//      return current + ' of ' + pages;
+
+//      switch(idx) {
+//        case 0:
+//          return '1 of ' + pages;
+//        case 1:
+//          return '';
+//      }
+//
+//      if(isOdd(idx)) {
+//        return '? of ' + pages;
+//      }
     }
-  }]);
+    var clientCurrent = 0,
+      pageBreak= 'page-break';
+    $scope.pageBreak = function(clients, cidx, client, pidx) {
+      var count = client.properties.length,
+        current = pidx + 1;
+//      $log.info('clients  ' + clients + ' idx ' + cidx +
+//        ' pidx ' + pidx + ' count ' + count);
+
+      if(current === count) {
+        return '';
+      }
+
+      return pageBreak;
+    }
+
+    $scope.cpageBreak = function(clients, cidx, client, pidx) {
+      var count = clients,
+        current = cidx + 1;
+      $log.info('cpageBreak current  ' + current + ' count ' + count);
+
+      if(current === count) {
+        return '';
+      }
+      return pageBreak;
+    }
+  });
