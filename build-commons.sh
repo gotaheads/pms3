eb6a75ee80bbc0501342411eb8a6af89fc9fcdd0
gotaheads <gotahiroki@gmail.com>
2018-05-15 09:50:43 +1000

    removed client based select

--- build-commons.sh
+++ build-commons.sh
@@ -4,5 +4,5 @@ function copy-env {
   SOURCE=$1
   TARGET=$2
   echo "cp $SOURCE $TARGET"
-  cp $SOURCE $TARGET || { echo "cp failed"; exit 1 }
+  cp $SOURCE $TARGET || { echo "cp failed"; exit 1; }
 }
