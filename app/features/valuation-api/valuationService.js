f2f41e0db53ab9525180708674170bb6befc14e7
gotaheads <gotahiroki@gmail.com>
2018-05-15 11:46:57 +1000

    moved auth to select page and then check auth prior to pdf download

--- app/features/valuation-api/valuationService.js
+++ app/features/valuation-api/valuationService.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('pms3App')
-  .service('valuationService', ['$http', '$log',
-    function valuationService($http, $log) {
+  .service('valuationService', ['$http', '$log', '$q',
+    function valuationService($http, $log, $q) {
       $log.info('start valuationService ');
 
       var createUrl = function(context) {
@@ -13,17 +13,24 @@ angular.module('pms3App')
       var valuationService = {}
       ;
 
-      valuationService.generatePdf = function(year, number) {
-        $log.info('valuationService.generatePdf year: ', year, ', number: ', number);
+      valuationService.pdf = function(year, number) {
+        $log.info('valuationService.pdf year: ', year, ', number: ', number);
         var url = createUrl('/generators?year=' + year + '&number=' + number);
-        return valuationService.isAuthenticated.then(function(authenticated) {
-
-          return $http.get(url, { withCredentials: true });
+        return valuationService.isAuthenticated().then(function(authenticated) {
+          return authenticated? url: $q.reject(false);
         })
-        .then(function (res) {
-          return res.data.authenticated;
-        });
       }
+      //
+      // valuationService.generatePdf = function(year, number) {
+      //   $log.info('valuationService.generatePdf year: ', year, ', number: ', number);
+      //   var url = createUrl('/generators?year=' + year + '&number=' + number);
+      //   return valuationService.isAuthenticated().then(function(authenticated) {
+      //     return $http.get(url, { withCredentials: true });
+      //   })
+      //   .then(function (res) {
+      //     return res.data.authenticated;
+      //   });
+      // }
 
       valuationService.isAuthenticated = function() {
         $log.info('valuationService.isAuthenticated');
--- app/features/valuations-by-landlord/selectValuations.js
+++ app/features/valuations-by-landlord/selectValuations.js
@@ -12,15 +12,24 @@ angular.module('pms3App')
 
     reportService.loadSelection($scope);
 
-    $scope.update = function(val) {
-      $log.info('' + $scope.clients +' '+ $scope.bulk + ' val' + val);
-      $scope.actions = reportService.loadActions($scope.clients, val);
-    };
+    // $scope.update = function(val) {
+    //   $log.info('' + $scope.clients +' '+ $scope.bulk + ' val' + val);
+    //   $scope.actions = reportService.loadActions($scope.clients, val);
+    // };
 
     valuationService.isAuthenticated().then(authenticated => {
       $log.info('SelectValuationsByLandlordCtrl authenticated: ', authenticated)
       $scope.authenticated = authenticated;
     })
 
+    $scope.pdf = function(year, number) {
+      $log.info('SelectValuationsByLandlordCtrl year: ', year, ', number: ', number);
+      valuationService.pdf(year, number).then(url => {
+        $log.info('SelectValuationsByLandlordCtrl url: ', url)
+        window.location.href = url;
+      }).catch(function (err) {
+        $scope.authenticated = false;
+      });
+    };
 
   }]);
